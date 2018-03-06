fb73257c99c481e5405ae299f8d065f99052b1a7
giocee <31427610+giocee@users.noreply.github.com>
2018-03-06 13:32:51 -0600

    Fix pending migration error
    $ rake db:drop
    
    $ rake db:create
    
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228175848) do
+ActiveRecord::Schema.define(version: 20180306031926) do
 
   create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
