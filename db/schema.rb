0ac0015b4f18175ba44930f4e851fe0832ed2a3f
giocee <31427610+giocee@users.noreply.github.com>
2018-03-06 19:36:34 -0600

    Add second Comment model
    $ rails g scaffold Comment body:string author:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306233736) do
+ActiveRecord::Schema.define(version: 20180307012834) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
