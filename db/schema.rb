96dd7514a0af8c2ce48341ad2d8389556743412f
giocee <31427610+giocee@users.noreply.github.com>
2018-03-06 18:10:12 -0600

    Add field link to Post model
    $ rails g migration AddLinkToPost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306031926) do
+ActiveRecord::Schema.define(version: 20180306233736) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180306031926) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
 end
